69e60c01f0dc0e898cf7e9e3e1086a068579f5c6
UIT-23521815 <23521815@gm.uit.edu.vn>
2025-05-02 08:21:43 +0700

    Giao diện đăng tải tài liệu, mô tả tài liệu, hoàn thành đăng tải

---
+++ Frontend/js/upload1.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    const menu = document.getElementById("side-nav");
+    menu.classList.toggle("active");
+    document.body.classList.toggle("sidebar-open");
+}
+
+function toggleSubmenu(element) {
+    const subMenu = element.nextElementSibling;
+    if (subMenu && subMenu.classList.contains('sub-menu')) 
+    {
+        subMenu.style.display = (subMenu.style.display === 'flex') ? 'none' : 'flex';
+
+        const icon = element.querySelector('i');
+        if (icon) {
+            icon.classList.toggle('fa-chevron-down');
+            icon.classList.toggle('fa-chevron-up');
+        }
+    }
+}
+
+
+
