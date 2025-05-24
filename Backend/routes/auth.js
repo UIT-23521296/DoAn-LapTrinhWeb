ca162c7879d741c2ad26b85397e61db92ba45750
UIT.23521296 <23521296@gm.uit.edu.vn>
2025-05-24 19:18:36 +0700

    Xử lý phân quyền user/admin/non-user (chưa hoàn thành)

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController =  require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+const path = require('path');
 
 //Đăng ký
 router.post('/register', authController.register);
@@ -16,5 +18,22 @@ router.post('/send-otp', authController.sendOtp);
 
 //Kiểm tra otp
 router.post('/verify-otp', authController.verifyOtp);
+
+//Kiểm tra trạng thái đăng nhập
+router.post('/get-session', authController.getSession);
+
+// Route bảo vệ - chỉ cho user đã đăng nhập
+router.get('/dashboard', authMiddleware, (req, res) => {
+    res.sendFile(path.join(__dirname, '../Public/index.html'));
+});
+
+// Trả thông tin người dùng
+router.get('/api/user-info', authMiddleware, (req, res) => {
+    res.json({
+        username: req.session.user.username,
+        email: req.session.user.email
+    });
+});
+
 
 module.exports = router;
