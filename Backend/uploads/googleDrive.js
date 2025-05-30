5a21959cb695638fe972d5e54586aefec09231b2
UIT-23521602 <23521602@gm.uit.edu.vn>
2025-05-30 11:24:02 +0700

    sua gan het loi

--- Backend/uploads/googleDrive.js
+++ Backend/uploads/googleDrive.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 const path = require('path');
 const mime = require('mime-types');
 
-const KEYFILEPATH = path.join(__dirname, '../credentials.json'); // sửa lại đúng vị trí file của bạn
+const KEYFILEPATH = path.join(__dirname, '../credentials.json');
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
 const auth = new google.auth.GoogleAuth({
